3ae57f60654a3a75467b18c2d14249b5a8f9781c
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-03-30 09:06:54 +0100

    got array working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,35 +6,43 @@ import SvgIcon from '@jamescoyle/vue-icon'
 import { mdiBookmarkOutline, mdiPlus } from '@mdi/js'
 
 const newTask = ref('');
-const tasks = [];
-const isTaskExist = false;
-function addTask() {
-    this.isTaskExist = false;
-    const value = newTask && newTask.trim();
-    if (!value) {
-        return;
-    }
-    const isTaskExist = this.tasks.find((task) => task.title === value);
-    if (!isTaskExist) {
-        this.tasks.push({
-            title: this.newTask,
-            done: false,
-        });
-        this.newTask = "";
-    }
-    if (isTaskExist) {
-        this.isTaskExist = true;
-    }
+let counter = 0;
+const tasks = ref([]);
+
+const addTask = () => {
+  tasks.value.push({
+    id: counter++,
+    name: newTask.value,
+    bookmarked: false,
+    completed: false,
+  });
+  newTask.value = '';
+}
+
+const capitalize = (name) => {
+  const firstLetter = name[0].toUpperCase();
+  const rest = name.slice(1);
+  return firstLetter + rest;
+}
 
 
+const removeTask = (index) => {
+  todos.value.splice(index, 1);
+}
+
+
+const selected = ref(false)
+
+const toggleSelected = () => {
+  return !selected;
 }
 
 const path = ref('');
 </script>
 
 <template>
-<div class="w-[1280px] h-[832px] left-0 top-0  bg-yellow-300">
-    <div class="w-[600px] h-[648px] left-[340px] top-[29px] bg-[#79F193]">
+<div>
+    <div class="w-[600px] h-[648px] left-[340px] top-[29px] bg-gray-300">
         <div class="flex flex-row mb-6 bg-sky-400">
             <div class="todo w-[109px] h-[39px] bg-red-300" >TO-DO</div>
             <div class="button_on">All tasks</div>
@@ -42,99 +50,50 @@ const path = ref('');
         </div>
         <div class="bg-pink-400">
             <v-container fluid>
-                <p>{{ task }}</p>
+                <p>{{ newTask }}</p>
                 <v-row>
-                    <v-checkbox v-model="newTask" label="task1" color="black" value="task1" />
+                    <v-checkbox color="black" value="task1">
+                      <template #label>{{ newTask }}</template>
+                    </v-checkbox>
                     <v-icon :icon="mdiBookmarkOutline" />
                 </v-row>
                 <v-row>
-                    <v-checkbox v-model="newTask" label="task2" color="black" value="task2" />
-                    <v-icon :icon="mdiBookmarkOutline" />
-
+                  <li v-for="(task, index) in tasks">
+                    New task: {{ newTask }} // Index: {{ index }} // ID: {{ task.id }} // Task Name: {{ task.name }}
+                  </li>
                 </v-row>
 
                 <v-row>
-                    <!-- <v-list lines="three" select-strategy="classic"> -->
-
-      <!-- <v-list-item value="">
-        <template v-slot:prepend="{ isActive }">
-
-          <v-list-item-action start>
-            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-          </v-list-item-action>
-        </template>
-
-        <v-list-item-title>Notifications</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Notify me about updates to apps or games that I downloaded
-        </v-list-item-subtitle>
-      </v-list-item>
-
-      <v-list-item value="sound">
-        <template v-slot:prepend="{ isActive }">
-          <v-list-item-action start>
-            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-          </v-list-item-action>
-        </template>
-
-        <v-list-item-title>Sound</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Auto-update apps at any time. Data charges may apply
-        </v-list-item-subtitle>
-      </v-list-item>
-
-      <v-list-item value="widgets">
-        <template v-slot:prepend="{ isActive }">
-          <v-list-item-action start>
-            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-          </v-list-item-action>
-        </template>
-
-        <v-list-item-title>Auto-add widgets</v-list-item-title>
-
-        <v-list-item-subtitle>
-          Automatically add home screen widgets when downloads complete
-        </v-list-item-subtitle>
-      </v-list-item>
-    </v-list> -->
-
-
-
-
-
-
-                    <!-- <v-list-item> -->
-                        <!-- <v-list-item-content>
-                          <v-list-item-title> -->
-                            <v-text-field
-                            label="Add a new Task"
-                            variant="solo"
-                            class="m-5">
-
-
-                                <template #append-inner>
-                                    <svg-icon type="mdi" :path="mdiPlus"></svg-icon>
-                                </template>
-                            </v-text-field>
-                        <!-- </v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item> -->
-
-
-
-
-
-
+                  <v-list class="w-full h-[500px] m-0">
+                    <span v-if="tasks.length == 0">Add a task</span>
+                    <span v-else>Your Tasks</span>
+                      <v-list-item v-for="(task, index) in tasks">
+                        <v-list-item-action>
+                          <v-checkbox v-model="selected">
+                            <template #label>{{ capitalize(task.name) }}</template>
+                          </v-checkbox>
+                        </v-list-item-action>
+                      </v-list-item>
+                  </v-list>
+                </v-row>
+                <v-row>
+                  <v-text-field
+                  v-model="newTask"
+                  label="Add a new Task"
+                  variant="solo"
+                  class="m-5">
+                    <template #append-inner>
+                        <svg-icon type="mdi" :path="mdiPlus" @click.prevent="addTask"></svg-icon>
+                    </template>
+                  </v-text-field>
 
                 </v-row>
-                <svg-icon type="mdi" :path="path"></svg-icon>
+
+
 
             </v-container>
 
 
-  <!-- <svg-icon type="mdi" :path="path"></svg-icon> -->
 
 
 
