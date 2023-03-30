578a7985acc26191347dce616c8dec53646d8333
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-03-30 11:45:21 +0100

    checkbox v-model fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,7 @@ const addTask = () => {
     name: newTask.value,
     bookmarked: false,
     completed: false,
+    checked: false,
   });
   newTask.value = '';
 }
@@ -31,76 +32,73 @@ const removeTask = (index) => {
 }
 
 
-const selected = ref(false)
 
-const toggleSelected = () => {
-  return !selected;
-}
 
 const path = ref('');
 </script>
 
 <template>
-<div>
-    <div class="w-[600px] h-[648px] left-[340px] top-[29px] bg-gray-300">
-        <div class="flex flex-row mb-6 bg-sky-400">
-            <div class="todo w-[109px] h-[39px] bg-red-300" >TO-DO</div>
-            <div class="button_on">All tasks</div>
-            <div class="button_off">Bookmarked</div>
-        </div>
-        <div class="bg-pink-400">
-            <v-container fluid>
-                <p>{{ newTask }}</p>
-                <v-row>
-                    <v-checkbox color="black" value="task1">
-                      <template #label>{{ newTask }}</template>
-                    </v-checkbox>
-                    <v-icon :icon="mdiBookmarkOutline" />
-                </v-row>
-                <v-row>
-                  <li v-for="(task, index) in tasks">
-                    New task: {{ newTask }} // Index: {{ index }} // ID: {{ task.id }} // Task Name: {{ task.name }}
-                  </li>
-                </v-row>
-
-                <v-row>
-                  <v-list class="w-full h-[500px] m-0">
-                    <span v-if="tasks.length == 0">Add a task</span>
-                    <span v-else>Your Tasks</span>
-                      <v-list-item v-for="(task, index) in tasks">
-                        <v-list-item-action>
-                          <v-checkbox v-model="selected">
-                            <template #label>{{ capitalize(task.name) }}</template>
-                          </v-checkbox>
-                        </v-list-item-action>
-                      </v-list-item>
-                  </v-list>
-                </v-row>
-                <v-row>
-                  <v-text-field
-                  v-model="newTask"
-                  label="Add a new Task"
-                  variant="solo"
-                  class="m-5">
-                    <template #append-inner>
-                        <svg-icon type="mdi" :path="mdiPlus" @click.prevent="addTask"></svg-icon>
-                    </template>
-                  </v-text-field>
-
-                </v-row>
-
-
-
-            </v-container>
-
-
-
-
-
-            <AllTasks />
-            <Bookmarked />
-        </div>
+<div class="relative w-full h-full bg-red-600">
+  <!-- <div class="absolute w-[600px] h-[800px] top-1/2	left-1/2 -mt-[400px] mr-0 mb-0 -ml-[300px] bg-gray-300"> -->
+    <div class="absolute w-[600px] h-[800px]  bg-gray-300">
+    <div class="flex flex-row mb-6 bg-sky-400">
+      <div class="todo w-[109px] h-[39px] bg-red-300" >TO-DO</div>
+      <div class="button_on">All tasks</div>
+      <div class="button_off">Bookmarked</div>
     </div>
+    <div class="bg-pink-400">
+      <v-container fluid>
+        <p>{{ newTask }}</p>
+        <v-row>
+          <v-checkbox color="black" value="task1">
+            <template #label>{{ newTask }}</template>
+          </v-checkbox>
+          <v-icon :icon="mdiBookmarkOutline" />
+        </v-row>
+        <v-row>
+          <li v-for="(task, index) in tasks">
+            New task: {{ newTask }} // Index: {{ index }} // ID: {{ task.id }} // Task Name: {{ task.name }}
+          </li>
+        </v-row>
+
+        <v-row>
+          <v-list class="w-full h-[500px] m-0">
+            <span v-if="tasks.length == 0">Add a task</span>
+            <span v-else>Your Tasks</span>
+              <v-list-item v-for="(task, index) in tasks">
+                <v-list-item-action>
+                  <v-checkbox v-model="task.checked">
+                    <template #label>{{ capitalize(task.name) }}</template>
+                  </v-checkbox>
+                </v-list-item-action>
+              </v-list-item>
+          </v-list>
+        </v-row>
+        <v-row>
+          <v-text-field
+          v-model="newTask"
+          label="Add a new Task"
+          variant="solo"
+          class="m-5">
+            <template #append-inner>
+              <svg-icon type="mdi" :path="mdiPlus" @click.prevent="addTask"></svg-icon>
+            </template>
+          </v-text-field>
+
+        </v-row>
+
+
+
+      </v-container>
+
+
+
+
+
+        <AllTasks />
+        <Bookmarked />
+    </div>
+  </div>
 </div>
 
 </template>
