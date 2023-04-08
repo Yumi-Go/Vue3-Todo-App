4933aa3d8ea6fd84df0eca95da44b497f4bf8a2a
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-04-08 14:02:41 +0100

    tab status in local storage working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
 import { ref } from "vue";
+import { useLocalStorage, StorageSerializers } from '@vueuse/core';
 
 const isAllTasks = ref(true);
 const isBookmarked = ref(false);
 
+const storeTabStatus = useLocalStorage('tab', {allTab: isAllTasks.value, bookmarkedTab: isBookmarked.value});
+const getTabStatus = useLocalStorage("tab", null, { serializer: StorageSerializers.object });
+
+function saveTabStatus() {
+  storeTabStatus.value.allTab = isAllTasks.value;
+  storeTabStatus.value.bookmarkedTab = isBookmarked.value;
+}
+
+
+
 function allTasksButtonToggle() {
   isAllTasks.value = true;
   isBookmarked.value = false;
+  saveTabStatus();
 }
 
 function bookmarkedButtonToggle() {
   isAllTasks.value = false;
   isBookmarked.value = true;
+  saveTabStatus();
 }
 
 </script>
@@ -23,15 +36,15 @@ function bookmarkedButtonToggle() {
       <div class="w-[109px] h-[39px] text-red-700 text-left text-[32px] font-[700] leading-[39px] tracking-normal" >TO-DO</div>
       <div class="flex flex-row">
         <router-link :to="{ name: 'Home' }">
-          <div class="btn_all flex justify-center items-center" :class="{ btn_clicked: isAllTasks }" @click="allTasksButtonToggle">
+          <div class="btn_all flex justify-center items-center" :class="{ btn_clicked: getTabStatus.allTab }" @click="allTasksButtonToggle">
             <input type="radio" id="btn_allTasks" name="tabBtn" v-model="isAllTasks" class="hidden"/>
-            <label for="btn_allTasks" class="text-center"><router-link :to="{ name: 'Home' }">All Tasks</router-link></label>
+            <label for="btn_allTasks" class="text-center"><router-link :to="{ name: 'Home' }">{{getTabStatus.allTab}}</router-link></label>
           </div>
         </router-link>
         <router-link :to="{ name: 'Bookmarked' }">
-          <div class="btn_bookmarked flex justify-center items-center" :class="{ btn_clicked: isBookmarked }" @click="bookmarkedButtonToggle">
+          <div class="btn_bookmarked flex justify-center items-center" :class="{ btn_clicked: getTabStatus.bookmarkedTab }" @click="bookmarkedButtonToggle">
             <input type="radio" id="btn_bookmarked" name="tabBtn" v-model="isBookmarked" class="hidden"/>
-            <label for="btn_bookmarked" class="text-center">Bookmarked</label>
+            <label for="btn_bookmarked" class="text-center">{{getTabStatus.bookmarkedTab}}</label>
           </div>
         </router-link>
       </div>
