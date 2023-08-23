730e36fccb239554f1b0c489d3999239c4bf03cc
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-08-23 01:34:18 +0100

    tidy up codes

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -10,38 +10,20 @@ const { search } = useSearch();
 </script>
 
 <template>
-<v-container fluid class="flex justify-center items-center px-5">
-<v-row>
-    <v-text-field
-    v-model="search"
-    label="Search"
-    color="black"
-    bg-color="#FFEBEE"
-    single-line
-    hide-details
-    class="mb-1">
-        <template #append-inner>
-            <svg-icon type="mdi" :path="mdiMagnify"></svg-icon>
-        </template>
-    </v-text-field>
-</v-row>
-</v-container>
-</template>
-<template>
-<v-container fluid class="flex justify-center items-center px-5">
-<v-row>
-    <v-text-field
-    v-model="search"
-    label="Search"
-    color="black"
-    bg-color="#FFEBEE"
-    single-line
-    hide-details
-    class="mb-1">
-        <template #append-inner>
-            <svg-icon type="mdi" :path="mdiMagnify"></svg-icon>
-        </template>
-    </v-text-field>
-</v-row>
-</v-container>
+    <v-container fluid class="flex justify-center items-center px-5">
+        <v-row>
+            <v-text-field
+                v-model="search"
+                label="Search"
+                color="black"
+                bg-color="#FFEBEE"
+                single-line
+                hide-details
+                class="mb-1">
+                <template #append-inner>
+                    <svg-icon type="mdi" :path="mdiMagnify"/>
+                </template>
+            </v-text-field>
+        </v-row>
+    </v-container>
 </template>
