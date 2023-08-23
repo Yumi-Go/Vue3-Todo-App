8c9875140a61872fe81fdd1e7b983a9ee3ad8549
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-08-23 01:31:33 +0100

    tidy up codes

--- src/components/BookmarkedList.vue
+++ src/components/BookmarkedList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useLocalStorage, StorageSerializers } from '@vueuse/core';
+import { useLocalStorage } from '@vueuse/core';
 import { mdiAlertCircleOutline, mdiBookmarkMultiple, mdiCheckBold, mdiBookmark, mdiBookmarkOutline, mdiTrashCanOutline } from '@mdi/js'
 import { useTabStatus } from "../composables/useTabStatus";
 import { useSearch } from "../composables/useSearch"
@@ -14,43 +14,72 @@ const { deleteTask } = useDelete();
 </script>
 
 <template>
-<v-container fluid class="flex flex-col flex-1 px-5">
-    <v-row>
-        <v-list bg-color="pink" class="w-full flex-1 m-0 text-white">
-            <v-list-item v-if="getBookmarkedTasks().length < 1" class="font-bold">
-                <div class="flex flex-col justify-center items-center h-[500px]">
-                    <div>
-                        <v-icon :icon="mdiAlertCircleOutline" color="#E0E0E0" size="100" class="opacity-50"/>
+    <v-container fluid class="flex flex-col flex-1 px-5">
+        <v-row>
+            <v-list
+                bg-color="pink"
+                class="w-full flex-1 m-0 text-white"
+            >
+                <v-list-item
+                    v-if="getBookmarkedTasks().length < 1"
+                    class="font-bold"
+                >
+                    <div class="flex flex-col justify-center items-center h-[500px]">
+                        <div>
+                            <v-icon
+                                :icon="mdiAlertCircleOutline"
+                                color="#E0E0E0"
+                                size="100"
+                                class="opacity-50"
+                            />
+                        </div>
+                        <div>
+                            <span class="text-[#E0E0E0] opacity-80">
+                                Bookmarked Task Does Not Exist
+                            </span>
+                        </div>
                     </div>
-                    <div>
-                        <span class="text-[#E0E0E0] opacity-80">Bookmarked Task Does Not Exist</span>
-                    </div>
-                </div>
-            </v-list-item>        
-            <v-list-item v-else class="font-bold">
-                <v-icon :icon="mdiBookmarkMultiple" color="#FFF9C4"/>
-                <span class="text-[#FFF9C4] pl-2">Your Bookmarked Tasks</span>
-            </v-list-item>
-            <v-list-item
-            v-for="task in filteredTasks()"
-            class="hover:bg-red-300 hover:text-black font-bold pl-5 group">
-            <div class="flex justify-between">
-                <div>
-                    <v-icon :icon="mdiCheckBold" class="mr-2"/>
-                    {{ task.name }}
-                </div>
-                <div>
-                    <v-icon v-if="task.bookmarked" :icon="mdiBookmark" @click="unBookmarkTask(task.id)"/>
+                </v-list-item>        
+                <v-list-item
+                    v-else
+                    class="font-bold"
+                >
                     <v-icon
-                    class="invisible group-hover:visible"
-                    :icon="mdiTrashCanOutline"
-                    @click="deleteTask(allTasks.indexOf(task))"
+                        :icon="mdiBookmarkMultiple"
+                        color="#FFF9C4"
                     />
-                </div>
-            </div>
-                <v-divider thickness="3px"></v-divider>
-            </v-list-item>
-        </v-list>
-    </v-row>
-</v-container>
+                    <span class="text-[#FFF9C4] pl-2">
+                        Your Bookmarked Tasks
+                    </span>
+                </v-list-item>
+                <v-list-item
+                    v-for="task in filteredTasks()"
+                    class="hover:bg-red-300 hover:text-black font-bold pl-5 group"
+                >
+                    <div class="flex justify-between">
+                        <div>
+                            <v-icon
+                                :icon="mdiCheckBold"
+                                class="mr-2"
+                            />
+                            {{ task.name }}
+                        </div>
+                        <div>
+                            <v-icon
+                                v-if="task.bookmarked"
+                                :icon="mdiBookmark"
+                                @click="unBookmarkTask(task.id)"
+                            />
+                            <v-icon
+                                class="invisible group-hover:visible"
+                                :icon="mdiTrashCanOutline"
+                                @click="deleteTask(allTasks.indexOf(task))"
+                            />
+                        </div>
+                    </div>
+                    <v-divider thickness="3px"/>
+                </v-list-item>
+            </v-list>
+        </v-row>
+    </v-container>
 </template>
